e2c7ff7eea788c250764442f388d77d91445e50e
krislig <krislig@gmail.com>
2013-10-13 22:09:49 +0200

    installed and setup Selenium
    python script for selenium fixed to work with python 3

---
+++ fts/test_static_pages.py
@@ -0,0 +1,10 @@
+from functional_tests import FunctionalTest, ROOT
+
+class TestHomePage(FunctionalTest):
+
+	def test_can_view_home_page(self):
+
+		self.browser.get(ROOT + '/tukker/')
+
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('Messages with 300 Chars', body.text)
