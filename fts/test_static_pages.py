5f5bedc4cc054d13b1f941ea85da6034e8c23a6e
krislig <krislig@gmail.com>
2013-10-13 23:19:10 +0200

    new test for our home page

--- fts/test_static_pages.py
+++ fts/test_static_pages.py
@@ -2,9 +2,18 @@ from functional_tests import FunctionalTest, ROOT
 
 class TestHomePage(FunctionalTest):
 
+	def setUp(self):
+		self.url= ROOT +'/tukker/'
+		get_browser = self.browser.get(self.url)
+
 	def test_can_view_home_page(self):
+		response_code = self.get_response_code(self.url)
+		self.assertEqual(response_code, 200)
 
-		self.browser.get(ROOT + '/tukker/')
+	def test_has_right_title(self):
+		title=self.browser.find_element_by_tag_name('title')
+		self.assertEqual('Microposts on Steroids', title.text)
 
+	def test_has_right_heading(self):
 		body = self.browser.find_element_by_tag_name('body')
 		self.assertIn('Message with 300 Chars', body.text)
